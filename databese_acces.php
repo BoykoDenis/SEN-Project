72f2d585b7885e46f24d5500af4f3d39bae9f41f
Denys <denya.boyko@gmail.com>
2020-12-08 18:05:24 +0100

    db acces changed

---
+++ databese_acces.php
@@ -0,0 +1,10 @@
+<?php
+
+define(DB_HOST, "localhost")
+define(DB_USER, "root")
+define(DB_PASS, "6iPxoaCC")
+define(DB_DBNAME, "boykovolf")
+
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_DBNAME, 3306);
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 
+<?php
+    require("database_acces.php");
+
+?>
+
 
 <html>
     <head>
