3c764cbb123c55af5b6d87c06aee5f4fb82daaca
Denys <denya.boyko@gmail.com>
2020-12-10 14:42:20 +0100

    suggested books in main connected to php books; No suggesting system

--- index.php
+++ index.php
@@ -25,12 +25,33 @@
         <div id = "main_div">
             <div class = "packs" id = "categories">
                 <?php
-                    $categories_sql = "SELECT "
+                    $categories_sql = "SELECT GenreID, GenreName FROM genres";
+                    $result = $con->query($categories_sql);
+                    while ($row = $result->fetch_assoc())
+                    {
+                        echo "<div class = 'category'>".$row['GenreName']."</div>";
+                    }
                 ?>
             </div>
             <div id = "main">
-                blbosti
-            </div>
+                <div class = "main_header" id = "recomended">
+                </div>
+                <div class = "main_header" id = "top">
+                </div>
+                <div class = "main_header" id = "placeholder">
+                </div>
+                <div class = "main_header" id = "sortby">
+                </div>
+                    <?php
+                            $sugg_books_sql = "SELECT BookID, Bookname FROM books LIMIT 5";
+                            $result = $con->query($sugg_books_sql);
+                            while ($row = $result->fetch_assoc())
+                            {
+                                echo '<div class = "suggestions">'.$row['Bookname'].'</div>';
+                            }
+                    ?>
+
+                </div>
             <div id = "adds">
                 reklamy
             </div>
